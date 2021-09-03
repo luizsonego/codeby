b10a5ff203212fe92f86a5a4856f90abe84907eb
luiz <leesonego@gmail.com>
2021-09-03 11:02:33 -0300

    created component Empty

---
+++ src/Components/Empty/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { IsEmpty } from './styles'
+
+function Empty() {
+  return (
+    <IsEmpty>
+      Seu carrinho esta vazio
+    </IsEmpty>
+  )
+}
+
+export default Empty;
---
+++ src/Components/Empty/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const IsEmpty = styled.h5`
+  font: 800 2.5em Poppins, sans-serif;
+  text-align: center;  
+  margin-top: 15px;
+  &:after {
+    content: " 🥺 ";
+  }
+`
